aef883603410a5e7a401f6dd9bb1494c0d8025b3
MinhNhatVuong <minh.vuongnhat@hcmut.edu.vn>
2026-03-20 22:59:29 +0700

    Create BK Smart Home project

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("BK SmartHome API running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
